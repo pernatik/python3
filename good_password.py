0bf61f11b028638ebed86cffeba23429a324cd37
Andrey Kovalev <grafkovalev@yandex.ru>
2022-11-29 17:01:19 +0300

    Update good_password.py

--- good_password.py
+++ good_password.py
@@ -5,15 +5,22 @@ def is_password_good(password):
     is_digit = False
     
     if len(password) < 8:
-        print('False')
+        return False
     else:
         for i in password:
-
-   
-
+            if i.isupper() == True:
+                is_upper = True
+            elif i.islower() == True:
+                is_lower = True
+            elif i in '0123456789':
+                is_digit = True
+        if is_digit == True and is_lower == True and is_upper == True:
+            return True
+        else:
+            return False
 
 # считываем данные
-#txt = input()
-txt = 'aabbCC11OP'
+txt = input()
+#txt = 'aaAA12qqp'
 # вызываем функцию
 print(is_password_good(txt))
