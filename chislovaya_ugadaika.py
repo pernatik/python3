5c1ddcccb9c1297749b268f9b2b741b37140abf3
Andrey Kovalev <grafkovalev@yandex.ru>
2022-11-30 17:12:04 +0300

    add число попыток chislovaya_ugadaika.py

--- chislovaya_ugadaika.py
+++ chislovaya_ugadaika.py
@@ -12,22 +12,23 @@ def is_valid(x):
 num = r.randint(1,100)
 print('Добро пожаловать в числовую угадайку')
 print(num)
+
+popytki = [] # Список попыток пользователя
+
 while True:
     user_num = int(input())
     if is_valid(user_num) == True:
+        popytki.append(user_num)
         
         if user_num < num:
             print('Ваше число меньше загаданного, попробуйте еще разок')
         if user_num > num:
             print('Ваше число больше загаданного, попробуйте еще разок')
         if user_num == num:
-            print('Вы угадали, поздравляем!')
-            print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+            #print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+            print(f'Вы угадали, поздравляем! Число попыток {len(popytki)}')
             break
 
     else:
         print('А может быть все-таки введем целое число от 1 до 100?')
 
-
-#print(is_valid(num))
-#print (math.ceil(math.log2(20)))
